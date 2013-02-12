0788e4fdbc0e75ad913cbec12cc7642ff4e6eebe
wilkie <wilkie05@gmail.com>
2013-02-11 23:23:22 -0500

    Adds Chapter accessors and specs.

--- gutenberg.gemspec
+++ gutenberg.gemspec
@@ -18,4 +18,5 @@ Gem::Specification.new do |gem|
   gem.require_paths = ["lib"]
 
   gem.add_dependency "tilt"
+  gem.add_dependency "redcarpet"
 end
--- spec/chapter_spec.rb
+++ spec/chapter_spec.rb
@@ -7,5 +7,22 @@ describe Gutenberg::Chapter do
   it "can be created with no arguments" do
     Gutenberg::Chapter.new.must_be_instance_of Gutenberg::Chapter
   end
+
+  it "can be created with a title" do
+    Gutenberg::Chapter.new({:title => "Book Chapter"}).title.must_equal("Book Chapter")
+  end
+
+  it "can be created with an author" do
+    Gutenberg::Chapter.new({:authors => ["wilkie"]})
+      .authors.first.must_equal("wilkie")
+  end
+
+  it "has a reasonable default title" do
+    Gutenberg::Chapter.new.title.must_equal("Untitled")
+  end
+
+  it "has a empty list of authors by default" do
+    Gutenberg::Chapter.new.authors.must_equal([])
+  end
 end
 
