71d0a3b76f0d5be455ef7071d27d4ea0840ea886
wilkie <wilkie05@gmail.com>
2013-02-16 11:14:40 -0500

    Moves helpers to HTMLSupervisor and updates views to use HTMLSupervisor.

--- lib/gutenberg/html_supervisor.rb
+++ lib/gutenberg/html_supervisor.rb
@@ -8,5 +8,14 @@ module Gutenberg
     def initialize(book)
       @book = book
     end
+
+    def format_outline(outline)
+      return "" if outline.nil?
+      if outline.text == "References"
+        "</ul><li><a href='##{outline.slug}'>#{outline.text}</a></li>#{format_outline(outline.sibling)}<ul>"
+      else
+        "<li><a href='##{outline.slug}'>#{outline.text}</a><ul>#{format_outline(outline.child)}</ul></li>#{format_outline(outline.sibling)}"
+      end
+    end
   end
 end
